7bb745201a350bb6ca88aaa7c4c494b7c8de182f
Nirmala2599 <115460529+Nirmala2599@users.noreply.github.com>
2023-03-15 21:36:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "NIRMALA T",
+      "email": "nirmalat931@gamil.com",
+      "phone": 6380044732,
+      "degree": "MCA",
+      "location": {
+        "address": "43/45 ANNA STREET VETTAVALAM",
+        "pinCode": 606754,
+        "city": "",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/nirmala-t-4b8420222/",
+          "github":"https://github.com/Nirmala2599"
+        }
+      ]
+    },
+    "Course": [
+      {
+        "company": "Guvi",
+        "position": "Full stock developmemt",
+        "startDate": "2023-03-01",
+        "endDate": "2023-03-15",
+        
+      },
+    ],
+    "education": [
+      {
+        "institution": "Arunai Engineering College  ",
+        "department": "MCA",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2021,
+        "gpa": 8.8,
+      }
+    ],
+    "skills": [
+      {
+        "name": "html,javascript",
+        "level": "beginer",
+        "project": [
+          "student attnace management bio metrick"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,drawing",
+      }
+    ]
+  }
+  console.log(myResume);
